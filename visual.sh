2db59258144d8cc6f2ace92a3f978a4a1e84359d
lucid327 <lucid@theori.io>
2025-09-18 23:14:59 -0500

    chore: code cleanup (viz & scripts)

--- visual.sh
+++
@@ -1,5 +0,0 @@
-$ PYTHONPATH=$(pwd) python visual/graph.py
-$ PYTHONPATH=$(pwd) python visual/graph_combine.py
-
-$ PYTHONPATH=$(pwd) python visual_llm/graph.py
-$ PYTHONPATH=$(pwd) python visual_llm/graph_combine.py
--- visual_clean.sh
+++
@@ -1,9 +0,0 @@
-rm results/*
-
-# rm save/objects/*
-rm save/avg_objects/*
-rm save/combined/iid/*
-rm save/combined/non_iid/*
-rm save/acc/*
-rm save/loss/*
-rm save/*
